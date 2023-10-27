dcf4f3748dbd06bf84c513ef3af14e563de0597b
ABE6194 <148538131+ABE6194@users.noreply.github.com>
2023-10-27 18:37:54 +0900

    Add files via upload

---
+++ 20231027-課題1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, result;
+
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	scanf_s("%d", &a);
+
+	result = a * a;
+	printf("%d*%d=%d\n", a, a, result);
+
+	return 0;
+}
