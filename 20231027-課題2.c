b301fb39d9a4000c6add2897dd1e1ddff11910dd
ABE6194 <148538131+ABE6194@users.noreply.github.com>
2023-10-27 18:44:05 +0900

    Add files via upload

---
+++ 20231027-課題2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, result;
+
+	printf("”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+	scanf_s("%d", &a);
+
+	if (a % 2 == 0) 
+	{printf("%d‚Í‹ô”‚Å‚ ‚é\n", a);}
+	else { printf("%d‚ÍŠï”‚Å‚ ‚é\n", a); }
+	
+	
+
+	return 0;
+}
